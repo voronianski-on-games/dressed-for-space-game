723ce6b3a6945cf8d1ff9acf8e14c70c121dc40a
Dmitri Voronianski <dmitri.voronianski@gmail.com>
2017-01-12 16:36:03 +0100

    try tweening

--- src/states/menu.lua
+++ src/states/menu.lua
@@ -1,26 +1,39 @@
+local _ = require('src/common')
+local Timer = require('vendor/timer')
 local media = require('src/media')
 local shaders = require('src/shaders')
 
 local menu = {}
+local title = nil
 
 function menu:init ()
 
 end
 
-function menu:update (dt)
+function menu:enter ()
+  local ww, wh = love.graphics.getDimensions()
 
+  title = {
+    x = ww / 2 - 260,
+    y = -60,
+    text = 'DRESSED FOR SPACE'
+  }
+
+  Timer.tween(5, title, {y = 100}, 'in-bounce')
+end
+
+function menu:update (dt)
+  Timer.update(dt)
 end
 
 function menu:draw ()
-  local ww, wh = love.graphics.getDimensions()
-
   shaders.postEffect():draw(function ()
-    shaders.postEffectBlurry():draw(function ()
-      media.drawBackgroundImage()
-    end)
+    -- shaders.postEffectBlurry():draw(function ()
+    --   media.drawBackgroundImage()
+    -- end)
 
     love.graphics.setFont(media.imageFontTitle)
-    love.graphics.print('DRESSED FOR SPACE', ww / 2 - 260, 100)
+    love.graphics.print(title.text, title.x, title.y)
   end)
 end
 
